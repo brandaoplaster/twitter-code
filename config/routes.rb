aa1da1eadac9480f35ac5f8bede29efe6e8ed0e2
brandaoplaster <brandaoplaster@gmail.com>
2020-04-27 21:58:42 -0300

    Adding controller Tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,36 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'tweets/index'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'tweets/show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'tweets/create'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'tweets/destroy'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'tweets/update'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       get 'users/create'
     end
   end
