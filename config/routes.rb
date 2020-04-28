6c5901fedc39a5b3e1646a56f30017dd806fecde
brandaoplaster <brandaoplaster@gmail.com>
2020-04-27 22:01:43 -0300

    Adding controller Follows

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'follows/create'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'follows/destroy'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       get 'likes/create'
     end
   end
