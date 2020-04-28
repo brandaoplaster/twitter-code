aa5f242e69658e0f774f645a75ff5a112b2fabce
brandaoplaster <brandaoplaster@gmail.com>
2020-04-27 21:57:27 -0300

    Adding controller Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,47 @@
 Rails.application.routes.draw do
   namespace :api do
+    namespace :v1 do
+      get 'users/create'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'users/destroy'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'users/update'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'users/current'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'users/show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'users/following'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'users/followers'
+    end
+  end
+
+  namespace :api do
     namespace :v1  do
       post 'user_token' => 'user_token#create'
     end
