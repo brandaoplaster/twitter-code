6f1607fd6076d07da859da9b9678268369d1126b
brandaoplaster <brandaoplaster@gmail.com>
2020-05-01 21:42:38 -0300

    Testing controller User action followers

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -246,5 +246,40 @@ RSpec.describe "Api::V1::Users", type: :request do
   end
 
   describe 'GET /api/v1/users/:id/followers?page=:page' do
+    context 'user exists' do
+      let(:user) { create(:user) }
+      let(:followers_number) { Random.rand(15..25) }
+
+      before { followers_number.times { create(:user).follow(user) } }
+
+      it do
+        get "/api/users/#{user.id}/followers?page=1"
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns right followers' do
+        get "/api/v1/users/#{user.id}/followers?page=1"
+        expect(json).to eql(each_serialized(Api::V1::UserSerializer, user.followers[0..14]))
+      end
+
+      it 'returns 15 elements on first page' do
+        get "/api/v1/users/#{user.id}/followers?page=1"
+        expect(json.count).to eql(15)
+      end
+
+      it 'returns remaining elements on second page' do
+        get "/api/v1/users/#{user.id}/followers?page=2"
+        remaining = user.followers.count - 15
+        expect(json.count).to eql(remaining)
+      end
+    end
+
+    context 'user dont exist' do
+      let(:user_id) { -1 }
+
+      before { get "/api/v1/users/#{user_id}/followers" }
+
+      it { expect(response).to have_http_status(:not_found) }
+    end
   end
 end
