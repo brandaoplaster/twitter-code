ca276cdf907e6383cea95fdbed7ed87335bb4922
brandaoplaster <brandaoplaster@gmail.com>
2020-05-01 21:06:27 -0300

    Testing controller User action following

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -208,6 +208,41 @@ RSpec.describe "Api::V1::Users", type: :request do
   end
 
   describe 'GET /api/v1/users/:id/following?page=:page' do
+    context 'user exists' do
+      let(:user) { create(:user) }
+      let(:following_number) { Random.rand(15..25) }
+
+      before { following_number.times { user.follow(create(:user)) } }
+
+      it do
+        get "/api/v1/users/#{user.id}/following?page=1"
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns right following' do
+        get "/api/v1/users/#{user.id}/following?page=1"
+        expect(json).to eql(each_serialized(Api::V1::UserSerializer, user.following_users[0..14]))
+      end
+
+      it 'returns 15 elements on first page' do
+        get "/api/v1/users/#{user.id}/following?page=1"
+        expect(json.count).to eql(15)
+      end
+
+      it 'returns remaining elements on second page' do
+        get "/api/v1/users/#{user.id}/following?page=2"
+        remaining = user.following_users.count - 15
+        expect(json.count).to eql(remaining)
+      end
+    end
+
+    context 'user dont exist' do
+      let(:user_id) { -1 }
+
+      before { get "/api/v1/users/#{user_id}/following" }
+
+      it { expect(response).to have_http_status(:not_found) }
+    end
   end
 
   describe 'GET /api/v1/users/:id/followers?page=:page' do
