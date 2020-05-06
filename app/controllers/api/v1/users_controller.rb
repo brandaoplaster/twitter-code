cd1e7584f334876035bfc64ed27008f49c53aaec
brandaoplaster <brandaoplaster@gmail.com>
2020-05-06 20:21:32 -0300

    Implementing action create, responsible for creating a new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,12 @@ module Api
       before_action :set_page except: %i[follwers following create]
 
       def create
+        user = User.new(user_params)
+        if user.save
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
       end
 
       def destroy
