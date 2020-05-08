1e0bc6345c28350c5dee707a227b921a0fe1c691
brandaoplaster <brandaoplaster@gmail.com>
2020-05-07 21:54:12 -0300

    Syntax correction

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ApiController < ApplicationController
       include Knock::Authenticable
-      include CanCan::ControleAdditions
+      include CanCan::ControllerAdditions
     end
   end
 end
